b755c83cc8ff4144daf6693a29ce47db781dfe9b
DevOpsMentor7867 <devopsmentor.7867@gmail.com>
2025-03-08 05:54:46 +0500

    UPDATED FRONTEND APIS REMOVED node_modules and .env from git

--- Frontend/src/components/UI/label.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function Label({ children, className, ...props }) {
-  return (
-    <label className={`block mb-2 ${className}`} {...props}>
-      {children}
-    </label>
-  );
-}
